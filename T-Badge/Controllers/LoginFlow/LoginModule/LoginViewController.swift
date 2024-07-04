2caf5d3f5d301bee39b7d1e0bca6a12b87ab054f
Karim <121769957+T-e-i-l-s@users.noreply.github.com>
2024-07-04 15:11:23 +0300

    login page

--- T-Badge/Controllers/LoginFlow/LoginModule/LoginViewController.swift
+++ T-Badge/Controllers/LoginFlow/LoginModule/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 // MARK: - View Protocol
 protocol LoginViewProtocol: AnyObject {
@@ -15,8 +16,74 @@ protocol LoginViewProtocol: AnyObject {
 // MARK: - View Controller
 final class LoginViewController: UIViewController {
     
-    // UI
+    lazy var titleLabel: UILabel = {
+        let l = UILabel()
+        l.text = "Вход"
+        l.textColor = .label
+        l.font = .systemFont(ofSize: 28)
+        return l
+    }()
     
+    lazy var nameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Логин"
+        tf.backgroundColor = .secondarySystemBackground
+        tf.textColor = .label
+        tf.font = .systemFont(ofSize: 19)
+        tf.borderStyle = .roundedRect
+        return tf
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Пароль"
+        tf.backgroundColor = .secondarySystemBackground
+        tf.textColor = .label
+        tf.font = .systemFont(ofSize: 19)
+        tf.borderStyle = .roundedRect
+        return tf
+    }()
+    
+    lazy var acceptButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Войти", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.layer.cornerRadius = 50
+
+        var configuration = UIButton.Configuration.filled()
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15)
+        button.configuration = configuration
+        
+        return button
+    }()
+    
+    lazy var registerView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [
+            noAccountLabel,
+            createAccountButton
+        ])
+        sv.axis = .horizontal
+        sv.alignment = .center
+        sv.spacing = 4
+        return sv
+    }()
+    
+    lazy var noAccountLabel: UILabel = {
+        let l = UILabel()
+        l.text = "Нет аккаунта?"
+        l.textColor = .label
+        l.font = .systemFont(ofSize: 16)
+        return l
+    }()
+    
+    lazy var createAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Зарегистрироваться.", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        return button
+    }()
+
     var presenter: LoginPresenterProtocol?
 
     override func viewDidLoad() {
@@ -26,7 +93,39 @@ final class LoginViewController: UIViewController {
     }
     
     private func setupUI() {
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(titleLabel)
+        view.addSubview(nameTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(acceptButton)
+        view.addSubview(registerView)
+        
+        
+        titleLabel.snp.makeConstraints{ make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(20)
+        }
+        
+        nameTextField.snp.makeConstraints{ make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.centerY.equalToSuperview()
+        }
+        
+        passwordTextField.snp.makeConstraints{ make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(nameTextField.snp.bottom).inset(-10)
+        }
+        
+        registerView.snp.makeConstraints{ make in
+            make.bottom.equalToSuperview().inset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        acceptButton.snp.makeConstraints{ make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.bottom.equalTo(registerView.snp.top).inset(-20)
+        }
     }
 }
 
